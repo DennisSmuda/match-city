099755d63dd695176bfa3bddf92e143bbacddd9d
Dennis Smuda <smuda.dennis@gmail.com>
2026-01-17 18:35:44 +0100

    chore: add linting to project

--- src/save-load.ts
+++ src/save-load.ts
@@ -1,28 +1,28 @@
-import { gameStore } from "./store";
-import { generateTile } from "./tile-generation";
+import { gameStore } from './store'
+import { generateTile } from './tile-generation'
 
-export const saveGame = () => {
-  localStorage.setItem("grid", JSON.stringify(gameStore.state.grid));
-  localStorage.setItem("score", JSON.stringify(gameStore.state.score));
-};
+export function saveGame() {
+  localStorage.setItem('grid', JSON.stringify(gameStore.state.grid))
+  localStorage.setItem('score', JSON.stringify(gameStore.state.score))
+}
 
-export const loadGame = async () => {
-  const loadedScore = localStorage.getItem("score") || ("0" as string);
-  const gridDataString = localStorage.getItem("grid") || ("{}" as string);
-  const grid = JSON.parse(gridDataString);
+export async function loadGame() {
+  const loadedScore = localStorage.getItem('score') || ('0' as string)
+  const gridDataString = localStorage.getItem('grid') || ('{}' as string)
+  const grid = JSON.parse(gridDataString)
 
   gameStore.set(() => ({
     grid,
-    score: parseInt(loadedScore),
-  }));
+    score: Number.parseInt(loadedScore),
+  }))
 
-  const scoreElement = document.getElementById("score") as HTMLElement;
-  scoreElement.innerHTML = loadedScore;
+  const scoreElement = document.getElementById('score') as HTMLElement
+  scoreElement.innerHTML = loadedScore
 
   for (const tilePos in grid) {
     if (Object.prototype.hasOwnProperty.call(gameStore.state.grid, tilePos)) {
-      const [x, y] = tilePos.split(":");
-      generateTile(grid[tilePos], parseInt(x), parseInt(y));
+      const [x, y] = tilePos.split(':')
+      generateTile(grid[tilePos], Number.parseInt(x), Number.parseInt(y))
     }
   }
-};
+}
--- src/store.ts
+++ src/store.ts
@@ -1,25 +1,26 @@
-import { makeStore } from "statery";
-import { AvailableThemes } from "./theming";
+import type { AvailableThemes } from './theming'
+import { makeStore } from 'statery'
 
 export const gameStore = makeStore<{
-  score: number;
-  chain: number;
-  grid: { [key: string]: any };
-  matches: { [key: string]: any };
-  mouse: { x: number; y: number };
-  currentTheme: AvailableThemes;
-  tutorialStep: number;
-  isAudioEnabled: boolean;
+  score: number
+  chain: number
+  grid: { [key: string]: any }
+  matches: { [key: string]: any }
+  mouse: { x: number, y: number }
+  currentTheme: AvailableThemes
+  tutorialStep: number
+  isAudioEnabled: boolean
 }>({
   score: 0,
   chain: 0,
   grid: {},
   matches: {},
   mouse: { x: 0, y: 0 },
-  currentTheme: "black",
+  currentTheme: 'black',
   tutorialStep: 0,
   isAudioEnabled: true,
-});
+})
 
-export const numberOfMatches = ({ matches }: any) =>
-  Object.keys(matches).length;
+export function numberOfMatches({ matches }: any) {
+  return Object.keys(matches).length
+}
